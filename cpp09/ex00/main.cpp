914b720c8ee3702cce994dd9743388d731ec3169
Elodie De Cola <ede-cola@f1r2s12.clusters.42paris.fr>
2025-07-10 17:53:25 +0200

    ex00 finished still cleaning to do

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -16,9 +16,10 @@ bool ft_check_date(std::string line)
 	double year = std::strtod(line.substr(0, line.find('-')).c_str(), &check);
 	if (*check)
 		return (0);
-	else if (day < 1 || day > 31 || month < 1 || month > 12 || year < 2009 || year > 2025)
+	else if (day < 1 || day > 31 || month < 1 || month > 12 || year < 2009 || year > 2025 || (month == 2 && day > 28 && year != 2012 && year != 2016 && year != 2020 && year != 2024))
 		return (0);
-	//check annee bisextile
+	else if (day == 29 && month == 2 && (year == 2012 || year == 2016 || year == 2020 || year == 2024))
+		return (1);
 	return (1);
 }
 
@@ -36,13 +37,12 @@ bool ft_check_extension(char *filename)
 
 void ft_trim(std::string &line)
 {
-	for (unsigned long i = 0; i < line.size(); i++)
+	for (size_t i = 0; i < line.size();)
 	{
 		if (isspace(line[i]))
-		{
-			line.erase(i);
-			i = 0;
-		}
+			line.erase(i, 1);
+		else
+			++i;
 	}
 }
 
@@ -68,6 +68,62 @@ void	ft_split_line(BitcoinExchange *btc, std::string line, bool &header)
 	else
 		btc->_values.insert(std::pair<std::string, double>(date, value));
 	
+}
+
+std::string getDate(std::string line, bool &header)
+{
+	if (line == "date|value" && !header)
+	{
+		header = 1;
+		return "\0";
+	}
+	else if (line == "date|value" && header)
+		throw std::runtime_error("Double header, invalid file");
+	else if (line.empty())
+		return "\0";
+	else
+	{
+		std::string date = line.substr(0, line.find('|'));
+		return (date);
+	}
+}
+
+double getValue(std::string line)
+{
+	if (line.empty())
+		return -1;
+	else
+	{
+		if (line.substr(line.find('|') + 1) != line && line != "date|value")
+		{
+			char *check;
+			double value = std::strtod(line.substr(line.find('|') + 1).c_str(), &check);
+			if (*check)
+				throw std::runtime_error("Incorrect value");
+			else if (value < 0)
+				throw std::runtime_error("Error: not a positive number.");
+			else if (value > 1000)
+				throw std::runtime_error("Error: too large a number.");
+			return (value);
+		}
+		else
+			return (1001);
+	}
+}
+
+void ft_compare(BitcoinExchange *btc, std::string date, double value)
+{
+	std::map<std::string, double>::iterator itLow = btc->_values.lower_bound(date);
+	for (std::map<std::string, double>::iterator it = btc->_values.begin(); it != btc->_values.end(); it ++)
+	{
+		if (it == itLow)
+		{
+			if (value < 1000)
+				std::cout << date << " => " << value << " = " << value * it->second << std::endl;
+			else
+				std::cerr << "Error: bad input => " << date << std::endl;
+		}
+	}
 }
 
 int main(int ac, char **av)
@@ -97,7 +153,6 @@ int main(int ac, char **av)
 		infile.open(av[1], std::fstream::in);
 		if (infile.is_open())
 		{
-			// check format "date | value" + check date valide .txt et .csv
 			BitcoinExchange *btc = new BitcoinExchange();
 			std::string line;
 			bool header = 0;
@@ -112,9 +167,33 @@ int main(int ac, char **av)
 					std::cerr << e.what() << '\n';
 				}
 			}
-			for (std::map<std::string, double>::iterator it = btc->_values.begin() ; it != btc->_values.end(); it++)
-				std::cout << it->first << " " << it->second << std::endl;
+			header = 0;
+			while (std::getline(infile, line))
+			{
+				try
+				{
+					ft_trim(line);
+					if (!line.empty())
+					{
+						std::string date = getDate(line, header);
+						double value = getValue(line);
+						if (!date.empty() && value >= 0)
+						{
+							if (ft_check_date(date) && value < 1001)
+								ft_compare(btc, date, value);
+							else
+								std::cerr << "Error: bad input => " << date << std::endl;
+						}
+					}
+				}
+				catch(const std::exception& e)
+				{
+					std::cerr << e.what() << '\n';
+				}
+				
+			}
 			data.close();
+			infile.close();
 			delete btc;
 		}
 		else
